35fb0f5c0527064922f71c03ec35019114a1a9e2
hdl <1371490392@qq.com>
2019-04-27 03:55:49 +0800

    修改了工程的一些错误

---
+++ TCMail/src/main/java/com/yc/TCMail/util/RedisUtil.java
@@ -0,0 +1,24 @@
+package com.yc.TCMail.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+public class RedisUtil {
+	@Autowired
+	private  StringRedisTemplate t;
+	
+	
+	public  void set(String key,String value) {
+		t.opsForValue().set(key,"value");
+	}
+	
+	public  String get(String key) {
+		return t.opsForValue().get(key);
+	}
+	
+	
+}
