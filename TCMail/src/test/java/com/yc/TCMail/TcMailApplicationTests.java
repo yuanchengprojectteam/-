b2161450946be49713f9aefcc5d536dcff5cd45b
hdl <1371490392@qq.com>
2019-04-27 15:23:25 +0800

    短信验证码已配置完成

--- TCMail/src/test/java/com/yc/TCMail/TcMailApplicationTests.java
+++ TCMail/src/test/java/com/yc/TCMail/TcMailApplicationTests.java
@@ -1,5 +1,11 @@
 package com.yc.TCMail;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpUtils;
+
+import org.apache.http.HttpResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.yc.TCMail.util.HttpUtil;
 import com.yc.TCMail.util.RedisUtil;
 
 @RunWith(SpringRunner.class)
@@ -24,5 +31,10 @@ public class TcMailApplicationTests {
 		/*
 		 * ru.set("a", "value"); System.out.println("==============="+ru.get("a"));
 		 */
+	}
+	
+	@Test 
+	public void SMS() {
+		HttpUtil.SMS("12345","15773283676");
 	}
 }
