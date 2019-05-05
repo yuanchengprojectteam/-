5dbf5043d33ba54f2117a59c82b234bb483741ee
BeYoung <BeYoung@SKY-20180919ZLL>
2019-05-05 17:45:09 +0800

    上传了分页配置

--- TCMail/src/main/java/com/yc/TCMail/TcMailApplication.java
+++ TCMail/src/main/java/com/yc/TCMail/TcMailApplication.java
@@ -1,11 +1,18 @@
 
 package com.yc.TCMail;
 
+import java.util.Properties;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import com.github.pagehelper.PageHelper;
+
+
 
 @SpringBootApplication
 @MapperScan(basePackages="com.yc.TCMail.dao")
@@ -22,4 +29,18 @@ public class TcMailApplication extends  SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(TcMailApplication.class, args);
 	}
+	
+	//配置mybatis的分页插件pageHelper
+    @Bean
+    public PageHelper pageHelper(){
+        PageHelper pageHelper = new PageHelper();
+        Properties properties = new Properties();
+        properties.setProperty("offsetAsPageNum","true");
+        properties.setProperty("rowBoundsWithCount","true");
+        properties.setProperty("reasonable","true");
+        //配置mysql数据库的方言
+        properties.setProperty("dialect","mysql");   
+        pageHelper.setProperties(properties);
+        return pageHelper;
+    }
 }
