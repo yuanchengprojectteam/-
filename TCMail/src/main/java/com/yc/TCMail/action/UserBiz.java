6b63ab478ea015099d67d000441065356e0feb32
xiaoyangmin <1493605854@qq.com>
2019-05-09 19:59:12 +0800

    Test

--- TCMail/src/main/java/com/yc/TCMail/action/UserBiz.java
+++ TCMail/src/main/java/com/yc/TCMail/action/UserBiz.java
@@ -66,6 +66,8 @@ public class UserBiz {
 		SimpleDateFormat  sdf=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 		Date  date=new Date();
 		user.setRegtime(sdf.format(date));
+		user.setType("普通用户");
+		user.setImage("http://doki-l.oss-cn-beijing.aliyuncs.com/UploadFile/header/h1.jpg");
 		return  uim.insertSelective(user);
 	}
 
